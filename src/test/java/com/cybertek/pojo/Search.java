f5e9a858a503fee60f254936dd8011e086423867
Igor Tirsina <igor.tirsina@yahoo.com>
2021-08-22 12:00:44 -0500

    day06, deserialization from JSON to POJO

--- src/test/java/com/cybertek/pojo/Search.java
+++ src/test/java/com/cybertek/pojo/Search.java
@@ -1,33 +1,18 @@
 package com.cybertek.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class Search {
 
     private List<Spartan> content;
     private int totalElement;
 
-    public List<Spartan> getContent() {
-        return content;
-    }
 
-    public void setContent(List<Spartan> content) {
-        this.content = content;
-    }
-
-    public int getTotalElement() {
-        return totalElement;
-    }
-
-    public void setTotalElement(int totalElement) {
-        this.totalElement = totalElement;
-    }
-
-    @Override
-    public String toString() {
-        return "Search{" +
-                "content=" + content +
-                ", totalElement=" + totalElement +
-                '}';
-    }
 }
