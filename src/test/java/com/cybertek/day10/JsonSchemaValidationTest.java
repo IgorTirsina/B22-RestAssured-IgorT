bce788f5ef338c47772b94d5b1bf623479c4372a
Igor Tirsina <igor.tirsina@yahoo.com>
2021-08-29 13:37:25 -0500

    day10, JsonSchemas added for allSpartans, PostSchema, SearchSchema

--- src/test/java/com/cybertek/day10/JsonSchemaValidationTest.java
+++ src/test/java/com/cybertek/day10/JsonSchemaValidationTest.java
@@ -6,6 +6,8 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,7 +30,26 @@ public class JsonSchemaValidationTest extends SpartanAuthTestBase {
                 .statusCode(200)
                 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json"))
                 .log().all();
+        //this method will work if the json schema will be under the resources
 
     }
+
+    @DisplayName("GET request to all spartans and verify schema")
+    @Test
+    public void allSpartanSchemaTest(){
+
+        given()
+                .accept(ContentType.JSON)
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                //what if you have your .json file not under resources following way -->
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/com/cybertek/day10/allSpartansSchema.json")));
+
+    }
+
+
 
 }
